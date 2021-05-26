b56f84a52f20fcf0b714c7b147e0df0a72333132
thomas.lemire123 <thomas.lemire123@icloud.com>
2021-05-26 10:41:07 -0400

    changed subscription

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,10 +2,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: EAAExNqrdVS4BACGWtN5eCxAavPOT1z7zjWgsZAP1O8o2cntEdTVZAkvtnOE7aYmgN5b7zvpiCb5FGgIZCIcKZAlMw9RdgEXh7p1aRiCqmdE0rTWntOZB2eELZAtC37rYLmAXe4mfDkc2gcSvVOZBz7gNKJu49cwPslUctMf3jneTLudO5C0HUPF,
-  subscribed_fields: %w[feed mention name]
-)
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Faceboook::Messenger::Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
